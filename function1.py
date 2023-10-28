2c43e382627c252e9ffaaa59368e0e9f93da0e3a
Sahil Kamath <sahilkamath0108@gmail.com>
2023-10-28 18:57:22 +0530

    commit 2

--- function1.py
+++ function1.py
@@ -15,33 +15,45 @@ pinecone.init(
 	api_key='b0b2574e-0973-49a8-a210-196772f9d1b2',      
 	environment='gcp-starter'      
 )      
-index = pinecone.Index('datahack')
+index = pinecone.Index('dh')
 
+global embeddings
+global model
+# embeddings = OpenAIEmbeddings(model='text-embedding-ada-002', show_progress_bar=True, openai_api_key='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')
+# embeddings = SentenceTransformerEmbeddings("sentence-transformers/multi-qa-mpnet-base-dot-v1")
 
-def create_index(path, index):
-    loader_cls = PyPDFLoader
-    loader = loader_cls(path)
+#pinecone.create_index("example-index", dimension=128, metric="euclidean", pods=4, pod_type="s1.x1")
+#pinecone.delete_index('dh')
+def create_index(path):
+    print('deleting')
+    pinecone.delete_index('dh')
+    print('creating')
+    pinecone.create_index('dh', dimension=768, metric='cosine')
+    print('loading')
+    loader = DirectoryLoader(path, glob="./*.pdf", loader_cls=PyPDFLoader)
     documents = loader.load()
+    # loader_cls = PyPDFLoader
+    # loader = loader_cls(path)
+    # documents = loader.load()
+    print('spliting')
    
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=200)
     texts = text_splitter.split_documents(documents)
-
+    print('embedding')
     embeddings = SentenceTransformerEmbeddings(model_name="multi-qa-mpnet-base-dot-v1")
-    db = Pinecone.from_documents(texts, embeddings, index_name = index)
+    print('storing')
+    db = Pinecone.from_documents(texts, embeddings, index_name = 'dh')
     print('done')
-    
-model = SentenceTransformer("sentence-transformers/multi-qa-mpnet-base-dot-v1")
+
 
 def get_ans(query):
-    embeddings = model.encode(query, convert_to_tensor=True)
-    print(type(embeddings.numpy()))
+    model = SentenceTransformer("sentence-transformers/multi-qa-mpnet-base-dot-v1")
+    embed = model.encode(query, convert_to_tensor=True)   #not for OAI
+    
+    # print(type(embed.numpy()))
 
-    fin = embeddings.numpy()[0]
-    # elements = fin.strip('[]').split()
-
-# Convert the elements to float and create a list
-    result_list = [float(element) for element in embeddings.numpy()[0]]
-    print(len(result_list))
+    result_list = [float(element) for element in embed.numpy()[0]]
+    print((result_list))
     resp = index.query(
         vector=result_list,
         top_k=3,
@@ -51,10 +63,11 @@ def get_ans(query):
     print(resp)
     #resp.matches[0,1,2] as per score
     
+def test():
+    print(pinecone.describe_index('dh'))
 
-    
-    
 if __name__ == '__main__':
-    path = "C:\\Users\\Hp\\Desktop\\data\\CASE OF KURT v. AUSTRIA.pdf"
-    # create_index(path, 'datahack')
-    get_ans(['The European Court of Human Rights, sitting as a Grand Chamber composed of:'])
+    path = "C:\\Users\\Hp\\Desktop\\Code\\Datahack\\case"
+    create_index(path)
+    print('done')
+    get_ans(['What was the outcome for the alleged violation of Article 8 in the case of ANAGNOSTAKIS v. GREECE?'])
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+l.encode([documents], convert_to_tensor=True)
+    # result_list = [float(element) for element in embeddings.numpy()[0]]
+    # print(result_list)
+    # print(len(result_list))
