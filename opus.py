2bdc1425880be08f2ee35054857d46ab2a981745
Jigar <95428432+jigarsiddhpura@users.noreply.github.com>
2023-10-29 05:46:54 +0530

    feat : array to pdf

--- opus.py
+++ opus.py
@@ -1,11 +1,17 @@
 from transformers import AutoTokenizer, MarianMTModel
 from langchain.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter,CharacterTextSplitter
-import os,asyncio
+import os,asyncio,time
 from PyPDF2 import PdfReader
 from typing_extensions import Concatenate
 
-sample_text = """36.  Le 9 juillet 2014, soit environ quatre ans et quatre mois plus tard, le 
+# pdf generation
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Paragraph
+from reportlab.lib.styles import getSampleStyleSheet
+
+
+sample_fr_text = """36.  Le 9 juillet 2014, soit environ quatre ans et quatre mois plus tard, le 
 tuteur H.K. saisit le tribunal d’instance de Nazilli pour demander la levée de 
 la mesure de tutelle. Rappelant que l’hôpital avait auparavant précisé qu’un 
 rétablissement de son épouse était possible sous traitement approprié 
@@ -14,6 +20,18 @@ plus  aucun  symptôme  franc  de  maladie.  Partant,  H.K.  demanda  que  deux
 hôpitaux  universitaires  soient  chargés  de  procéder  à  une  nouvelle  expertise 
 psychiatrique."""
 
+
+def create_pdf(text_array):
+    pdf_file = "translation.pdf"
+    doc = SimpleDocTemplate(pdf_file, pagesize=letter)
+
+    elements = []
+    styles = getSampleStyleSheet()
+    for text in text_array:
+        elements.append(Paragraph(text, styles["Normal"]))
+
+    doc.build(elements)
+    print(f"PDF generated: {pdf_file}")
 
 class TranslationService:
     def __init__(self, src, trg):
@@ -61,16 +79,23 @@ def get_documents(pdf_path):
 # path = os.path.join('Datahack2024','SearchEnginePDFs','pdfs','AFFAIRE A.A.K. c. TÜRKiYE.pdf')
 path = 'D:\\DATAHACK\\Datahack2024\\SearchEnginePDFs\\pdfs\\AFFAIRE A.A.K. c. TÜRKiYE.pdf'
 chunk_of_texts = get_documents(path)
+print(len(chunk_of_texts))
 
 print("translating")
+start = time.time()
 # fr_translate = asyncio.run(translate_texts(chunk_of_texts[:10]))
 translation_service = TranslationService(src="fr", trg="en")
 
-translation_tasks = [translation_service.translate_texts([text]) for text in chunk_of_texts[:10]]
+translation_tasks = [translation_service.translate_texts([text]) for text in chunk_of_texts[:5]]
 
 # Run the tasks asynchronously
 loop = asyncio.get_event_loop()
 translated_texts = loop.run_until_complete(asyncio.gather(*translation_tasks))
 
-print(translated_texts[0])
+end = time.time() - start
+print(f"------------------------ {end} ----------------------")
+
+create_pdf(translated_texts)
+
+
 
